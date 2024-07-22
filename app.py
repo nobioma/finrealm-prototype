2a64cadb97b4f4014ee961e1a6cfb6cad5dfada0
Nnanyere Obioma <nnanyereobioma@gmail.com>
2024-07-22 13:59:58 -0700

    Fixed routing in flask app to use ui pages

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('dashboard'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('sign-in.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
@@ -131,7 +131,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('sign-in'))
+        return redirect(url_for('login'))
     return render_template('sign-up.html', title='Register', form=form)
 
 @app.route('/dashboard')
